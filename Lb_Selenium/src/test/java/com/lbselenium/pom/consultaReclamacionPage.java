5abb40e8289b8f5ee5f962f9871a6aaf22c65e85
victor manuel alcantara <victorfrias2000@gmail.com>
2022-05-27 09:38:38 -0400

    nuevas libreria del pom y clase consultarReclamacionPage

---
+++ Lb_Selenium/src/test/java/com/lbselenium/pom/consultaReclamacionPage.java
@@ -0,0 +1,9 @@
+package com.lbselenium.pom;
+
+public class consultaReclamacionPage {
+    public static void main(String[] args) {
+        String nombreUsuario ="valcantara QA";
+        String apellidoUsuario="victor alcantara";
+        System.out.println("consultar reclamacion desde  INFOPLAN " + nombreUsuario);
+    }
+}
